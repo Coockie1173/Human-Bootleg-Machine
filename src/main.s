b21ef8822e6337263b4c2a1b9fcbc94e8aa31567
.Coockie <rudjerud@hotmail.com>
2025-10-27 21:50:51 +0100

    start defining stuff

--- src/main.s
+++ src/main.s
@@ -1,5 +1,9 @@
 .include "./boot/boot.s"
 .include "nmi.s"
+.include "controller/inputs.s"
+.include "memmap.s"
 
 main:
+    LDA #$01
+    STA SCRATCH0
     jmp main
---
+++ src/memmap.s
@@ -0,0 +1,22 @@
+.define SCRATCH0 $00
+.define SCRATCH1 $01
+.define SCRATCH2 $02
+.define SCRATCH3 $03
+.define SCRATCH4 $04
+.define SCRATCH5 $05
+.define SCRATCH6 $06
+.define SCRATCH7 $07
+.define SCRATCH8 $08
+.define SCRATCH9 $09
+.define SCRATCHA $0A
+.define SCRATCHB $0B
+.define SCRATCHC $0C
+.define SCRATCHD $0D
+.define SCRATCHE $0E
+.define SCRATCHF $0F
+
+.define CONADDR $10
+.define CONADDRPREV $11
+
+.define COMMANDS $0100 ;size 0xFF
+.define VARIABLES $0200 ;size 0xFF
