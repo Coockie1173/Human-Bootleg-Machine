973302b134711a97152c2a86efc6bb1cfbbc60a3
.Coockie <rudjerud@hotmail.com>
2025-11-10 11:14:05 +0100

    comments

--- src/interpreter/Puzzles.s
+++ src/interpreter/Puzzles.s
@@ -28,53 +28,53 @@ CheckAllSolutions:
     TXA
     ASL
     TAX
-    LDY #$03
+    LDY #$03 ;we have three "model" solutions to every puzzle - might get replaced in the future
     LDA #$00
-    STA VAR3
+    STA VARF ;clear the internal list index
 
 : 
-    PHY
-    PHX
+    PHY ;don't lose Y
+    PHX ;don't lose X
     LDA FullPuzzleList,x ;grab pointer to puzzlelist from the full list
     STA VAR1
     INX
     LDA FullPuzzleList,x
-    STA VAR0
+    STA VAR0 ;and store the pointer in VAR0 so we can access it later (below)
 
-    LDY VAR3
+    LDY VARF
     LDA (VAR0),y ;setup inbox pointer
     STA INBOXPTR+1
     INY
     LDA (VAR0),y
-    STA INBOXPTR
+    STA INBOXPTR ;setup our inbox pointer for our interpreter
 
     PLX
     LDA FullSolutionList,x ;grab pointer to solution from the full list
     STA VAR1 
     INX
     LDA FullSolutionList,x
-    STA VAR0
+    STA VAR0 
 
-    LDY VAR3
+    LDY VARF
     LDA (VAR0),y ;setup solution pointer
     STA CHECKSOLPTR+1
     INY
     LDA (VAR0),y
-    STA CHECKSOLPTR
+    STA CHECKSOLPTR ;setup our solution pointer for our solution checker
 
     INY 
-    STY VAR3
+    STY VARF ;increase and store our "global index" - DO NOT FRY!!
 
     LDA #$00
-    STA INTERPTR
+    STA INTERPTR ;and then reset our cursors
     STA SOLPTR
 
 :
-    JSR ParseInstruction ;parse all instructions
+    JSR ParseInstruction ;parse all instructions until the inbox is empty or something else fails
     BCC :-
 
-    PLY
-    DEY
+    PLY ;get our counter back
+    DEY ;decrement and check
     CPY #$00
-    BNE :--
+    BNE :-- ;if not 0, jump back and check the next solution
 RTS
