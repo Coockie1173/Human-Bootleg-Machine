7f12b3f479eb55d68bd61de3e6940c5540201fa0
Oumi-El-Khalfaoui <oumi.el.khalfaoui@student.howest.be>
2025-11-14 20:27:38 +0100

    Added test sprites, need to fix palettes

--- src/nmi.s
+++ src/nmi.s
@@ -7,18 +7,15 @@ nmi:
   PHY
 
 
-  ;ldx #$00 	; Set SPR-RAM address to 0
-  ;stx $2003
-;@loop:	lda testsprites, x 	; Load the sprites into SPR-RAM
-  ;sta $2004
-  ;inx
-  ;cpx #$5c
-  ;bne @loop
+  ldx #$00 	; Set SPR-RAM address to 0
+  stx $2003
+@loop:	lda testsprites, x 	; Load the sprites into SPR-RAM
+  sta $2004
+  inx
+  cpx #$5c
+  bne @loop
 
-; RESET SCROLL POSITION EVERY FRAME!
-  lda #$00
-  sta $2005  ; X scroll = 0
-  sta $2005  ; Y scroll = 0
+
 
   PLY
   PLX
@@ -30,15 +27,17 @@ testsprites:
 
   ; sprites
   ;for example:
-  ;.byte $00, $00, $00, $00 	; Dummy
-  ;.byte $00, $00, $00, $00
+  .byte $00, $00, $00, $00 	; Dummy
+  .byte $00, $00, $00, $00
 
-  ;.byte $20, $21, $00, $C0
-  ;.byte $20, $22, $00, $C8
-  ;.byte $10, $1F, $00, $B8
-  ;.byte $20, $23, $00, $D0
-  ;.byte $20, $23, $00, $D8
-  ;.byte $20, $23, $00, $E0
-  ;.byte $20, $23, $00, $E8
+  .byte $20, $21, $11, $C0
+  .byte $20, $22, $11, $C8
+  .byte $10, $1F, $11, $B8
+  .byte $20, $23, $11, $D0
+  .byte $20, $23, $11, $D8
+  .byte $20, $23, $11, $E0
+  .byte $20, $23, $11, $E8
   
+  ; pad remaining space
+  .res 256 - (* - testsprites), $00
 
