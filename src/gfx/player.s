9aaff32be74fc5824658bb512dcc27ac55379b63
Oumi-El-Khalfaoui <oumi.el.khalfaoui@student.howest.be>
2025-11-24 12:03:29 +0100

    Updated background and bg attributes, added Player sprites to tileset.

---
+++ src/gfx/player.s
@@ -0,0 +1,7 @@
+; NEED:
+; player position > Start and End
+; player sprites > walk cycle animation, idle animation?
+; speed > very fast
+
+
+;Initialize player at start idle pos $2063, $2064, $2083, $2084 > palette $00, not flipped
