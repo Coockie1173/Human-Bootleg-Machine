968108aa325628ed1bf82d056a2a3c71ea761b23
.Coockie <rudjerud@hotmail.com>
2025-12-11 15:20:02 +0100

    change mirroring

--- src/boot/header.s
+++ src/boot/header.s
@@ -1,5 +1,5 @@
 ;don't touch this, this is the NES rom header
 .segment "HEADER"
   ; .byte "NES", $1A      ; iNES header identifier
-  .byte $4E,$45,$53,$1A,$02,$01,$01,$08
+  .byte $4E,$45,$53,$1A,$02,$01,$00,$08
   .byte $00,$00,$07,$00,$01,$00,$00,$01
