002f38639d3608e8e37d77d6adac33568cc07e20
Kerem <kerem20emre@gmail.com>
2025-12-14 19:25:43 +0100

    basic level select

--- src/gfx/levelselect.s
+++ src/gfx/levelselect.s
@@ -1,5 +1,9 @@
-TEXTLEVELSELECT:
-    STRBYTE "LEVEL SELECT"
+TEXTLEVELSELECT: STRBYTE "LEVEL SELECT"
+
+TEXTLEVEL1: STRBYTE "ITS CORN"
+TEXTLEVEL2: STRBYTE "KIIRGHH"
+TEXTLEVEL3: STRBYTE "A BIG LUMB WITH KNOTS"
+TEXTLEVEL4: STRBYTE "IT HAS THE JUICE"
 
 init_levelselect:
     ;clear out the entire screen  and show a puzzle list
@@ -22,7 +26,7 @@ init_levelselect:
     lda #STATE_LEVEL_SELECT
     sta game_state
 
-    ;now add the text
+    ;now add the title
     ldx #$00
     lda #$20
     sta VAR0
@@ -31,14 +35,94 @@ init_levelselect:
     @drawtoptext:
         lda TEXTLEVELSELECT, x
         cmp #$FF
-        beq @done
+        beq :++
         cmp #$01
-        beq @NoDraw
+        beq :+
         sta VAR2
         jsr DrawLetter
-        @NoDraw:
+        :
         inc VAR1
         inx
     jmp @drawtoptext
-    @done:
+    :
+
+    ;now add the level 1
+    ldx #$00
+    lda #$20
+    sta VAR0
+    lda #$A6    ; 3 rows down, 4 tiles right
+    sta VAR1
+    @drawlevel1:
+        lda TEXTLEVEL1, x
+        cmp #$FF
+        beq :++
+        cmp #$01
+        beq :+
+        sta VAR2
+        jsr DrawLetter
+        :
+        inc VAR1
+        inx
+    jmp @drawlevel1
+    :
+
+    ;now add the level 2
+    ldx #$00
+    lda #$20
+    sta VAR0
+    lda #$E6    ; 3 rows down, 4 tiles right
+    sta VAR1
+    @drawlevel2:
+        lda TEXTLEVEL2, x
+        cmp #$FF
+        beq :++
+        cmp #$01
+        beq :+
+        sta VAR2
+        jsr DrawLetter
+        :
+        inc VAR1
+        inx
+    jmp @drawlevel2
+    :
+
+    ;now add the level 3
+    ldx #$00
+    lda #$21
+    sta VAR0
+    lda #$26   ; 3 rows down, 4 tiles right
+    sta VAR1
+    @drawlevel3:
+        lda TEXTLEVEL3, x
+        cmp #$FF
+        beq :++
+        cmp #$01
+        beq :+
+        sta VAR2
+        jsr DrawLetter
+        :
+        inc VAR1
+        inx
+    jmp @drawlevel3
+    :
+
+    ;now add the level 4
+    ldx #$00
+    lda #$21
+    sta VAR0
+    lda #$66   ; 3 rows down, 4 tiles right
+    sta VAR1
+    @drawlevel4:
+        lda TEXTLEVEL4, x
+        cmp #$FF
+        beq :++
+        cmp #$01
+        beq :+
+        sta VAR2
+        jsr DrawLetter
+        :
+        inc VAR1
+        inx
+    jmp @drawlevel4
+    :
 rts
